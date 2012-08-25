9d6f0f08da8d5b574cdc5d462c70461b12773192
cristianoprause <cristianoprause@gmail.com>
2012-08-25 00:49:19 -0300

    adicionadas mensagens para reiniciar e finalizar o sistema

---
+++ mecasoft/src/aplicacao/command/SairCommand.java
@@ -0,0 +1,20 @@
+package aplicacao.command;
+
+import static aplicacao.helper.MessageHelper.openQuestion;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.PlatformUI;
+
+public class SairCommand extends AbstractHandler{
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		if(openQuestion("Tem certeza que deseja finalizar o sistema?"))
+			return PlatformUI.getWorkbench().close();
+		
+		return null;
+	}
+
+}
